51d2bfd65537a34d53ae0bce697d64176d3f8d99
FredericKanni <kannif@ymail.com>
2019-10-16 13:17:56 +0400

    creation formulaire +  recuperation donnees avec POST

---
+++ minichat.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+
+<form action="minichat_post.php" method="post">
+        <p>
+        <label for="pseudo">Pseudo</label> : <input type="text" name="pseudo" id="pseudo" /><br />
+        <label for="message">Message</label> :  <input type="text" name="message" id="message" /><br />
+
+        <input type="submit" value="Envoyer" />
+	</p>
+    </form>
+
+
+
+    
+</body>
+</html>
---
+++ minichat_post.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+
+<?php
+// Effectuer ici la requête qui insère le message
+// Puis rediriger vers minichat.php comme ceci :
+//header('Location: minichat.php');
+?>
+
+
+
+<form  >
+        <p>
+        <label for="pseudo">Pseudo</label> : <input type="text" name="pseudo" id="pseudo" /><br />
+        <label for="message">Message</label> :  <input type="text" name="message" id="message" /><br />
+
+       
+	</p>
+    </form>
+
+
+
+    <?php
+        $pseudo = $_POST['pseudo'];
+        echo  $pseudo . '<br>'; 
+        $msg = $_POST['message'];
+        echo  $msg;
+    ?>
+    
+</body>
+</html>
